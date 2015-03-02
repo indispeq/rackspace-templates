a4d2083160d304197b421e69c972ef139a57e2fe
Aron Szekely <aron.szekely@people-doc.com>
2015-03-02 15:34:57 +0100

    working hook

--- salt_master.sh
+++ salt_master.sh
@@ -2,11 +2,14 @@
 
 su git -c 'git init --bare /var/git/novasalt'
 sudo git clone /var/git/novasalt /srv
+sudo chown -R git:git /srv
 su git -c 'cat <<EOF >/var/git/novasalt/hooks/post-update
 #!/bin/sh
 cd /srv/ || exit
 unset GIT_DIR
-sudo git pull origin master 
+git pull origin master 
 exec git-update-server-info
 #sudo salt '*' state.highstate
 EOF'
+su git -c 'chmod +x /var/git/novasalt/hooks/post-update'
+
