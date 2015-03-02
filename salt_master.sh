84d23778dea33afdada66107a5221857cfb99219
Aron Szekely <aron.szekely@people-doc.com>
2015-03-02 12:04:11 +0100

    add salt_master setup script

---
+++ salt_master.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo -u git -c 'git init --bare /var/git/novasalt'
+sudo git clone /var/git/novasalt /srv
+sudo -u git -c 'cat <<EOF >/var/git/novasalt/hooks/post-update'
+\#!/bin/sh
+exec git update-server-info
+cd /srv
+git pull
+\#sudo salt '*' state.highstate
+EOF
