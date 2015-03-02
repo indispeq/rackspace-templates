4e6baecca6e4e77359c42346f3bb16f06439da94
Aron Szekely <aron.szekely@people-doc.com>
2015-03-02 14:40:40 +0100

    fix post-update hook

--- salt_master.sh
+++ salt_master.sh
@@ -2,10 +2,10 @@
 
 su git -c 'git init --bare /var/git/novasalt'
 sudo git clone /var/git/novasalt /srv
-su git -c 'cat <<EOF >/var/git/novasalt/hooks/post-update'
-\#!/bin/sh
+su git -c 'cat <<EOF >/var/git/novasalt/hooks/post-update
+#!/bin/sh
 exec git update-server-info
 cd /srv
 git pull
-\#sudo salt '*' state.highstate
-EOF
+#sudo salt '*' state.highstate
+EOF'
